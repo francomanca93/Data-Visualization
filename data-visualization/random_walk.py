ba178f3befe4ac4bdf33b02e2edad5454c0fb225
francomanca93 <francomanca93@gmail.com>
2020-04-16 12:12:40 -0300

    Choosing Directions

--- data-visualization/random_walk.py
+++ data-visualization/random_walk.py
@@ -13,6 +13,32 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
     
+    def fill_walk(self):
+        """Calculate all the points in the walk."""
+
+        # Keep taking steps until the walk reaches the desired lenght.
+        while len(self.x_values) < self.num_points:
+
+            # Decide wich direction to go and how far to go in that direction.
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # Reject moves that go nowhere.
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calculate the new position.
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
 
 
 
