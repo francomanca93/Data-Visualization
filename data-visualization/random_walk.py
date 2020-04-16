157fe6c618b1f1f7a3d7db5c1528f212e105c37e
francomanca93 <francomanca93@gmail.com>
2020-04-16 12:11:31 -0300

    Creating the RandomWalk() Class

---
+++ data-visualization/random_walk.py
@@ -0,0 +1,23 @@
+from random import choice  # 
+
+class RandomWalk:
+    """ A class to generate random walks.
+        Una clase para generar caminos aleatorios.
+    """
+
+    def __init__(self, num_points=5000):
+        """Initialize attributes of a walk"""
+        self.num_points = num_points  # Variable to store the nomber of points in the walk
+
+        # All walks start at (0, 0). Two list to store coordinate of each point in the walk
+        self.x_values = [0]
+        self.y_values = [0]
+    
+
+
+
+
+
+
+
+
