1adda1cf97734e7c6478a35202a46c1a9d8307ef
francomanca93 <francomanca93@gmail.com>
2020-04-16 18:39:30 -0300

    Generating multiple random walks

--- data-visualization/randomWalk_visual.py
+++ data-visualization/randomWalk_visual.py
@@ -3,27 +3,30 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk
-rw = RandomWalk()  # We create a object from the RandomWalk class
-rw.fill_walk()  # We call the method fill_walk()
+# Keep making new walks, as long as the program is active
+while True: # <------- Comment this line and others that it have arrows if you'll run in jupyter
+    # Make a random walk
+    rw = RandomWalk()  # We create a object from the RandomWalk class
+    rw.fill_walk()  # We call the method fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('seaborn')
-fig = plt.subplot() 
-ax = plt.subplot()  
-ax.scatter(rw.x_values, rw.y_values, s=10)
+    # Plot the points in the walk.
+    plt.style.use('seaborn')
+    fig = plt.subplot() 
+    ax = plt.subplot()  
+    ax.scatter(rw.x_values, rw.y_values, s=10)
 
-# Set chart tittle and label axes.
-ax.set_title("Random Walks", fontsize=20)
-ax.set_xlabel("X values", fontsize=14)
-ax.set_ylabel("Y values", fontsize=14)
+    # Set chart tittle and label axes.
+    ax.set_title("Random Walks", fontsize=20)
+    ax.set_xlabel("X values", fontsize=14)
+    ax.set_ylabel("Y values", fontsize=14)
 
-# Set size of tick labels.
-ax.tick_params(axis='both', which='major', labelsize=10)
+    # Set size of tick labels.
+    ax.tick_params(axis='both', which='major', labelsize=10)
 
-plt.show()
+    plt.show()
 
-
-
+    keep_running = input("Make another walk? (y/n): ")  # <------- Comment this line if you'll run in jupyter
+    if keep_running == 'n':  # <------- Comment this line if you'll run in jupyter
+        break  # <------- Comment this line if you'll run in jupyter
 
 # %%
