c6bad30b0a2e237103938f1731f7ccf39ff8dd7e
francomanca93 <francomanca93@gmail.com>
2020-04-17 09:10:18 -0300

    Analyzing the Results. Dice roll frecuencies.

--- data-visualization/rolling_dice_with_Plotly/die_visual.py
+++ data-visualization/rolling_dice_with_Plotly/die_visual.py
@@ -5,8 +5,14 @@ die = Die()  # instace of Die class
 
 # Make some rolls, and store results in a list.
 results = []  # A list of results
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analize the results.
+frequencies = []
+for value in range(1, (die.num_sides + 1)):
+    frequency = results.count(value)  # .count return number of ocurrences of a value
+    frequencies.append(frequency)
+
+print(frequencies)
