1bba810edb9c1c6cdfdadcad31b33e904c16edc5
francomanca93 <francomanca93@gmail.com>
2020-04-18 10:11:54 -0300

    Parsing the CSV file headers

---
+++ downloading-data/sitka_highs.py
@@ -0,0 +1,11 @@
+import csv
+
+# filename = '/home/franco/Documents/Python/Proyectos/Data-Visualization/downloading-data/data/sitka_weather_07-2018_simple.csv'
+
+filename = 'data/sitka_weather_07-2018_simple.csv'  # String object
+with open(filename) as f:  # Open the file and assign the resulting file object to 'f'
+    reader = csv.reader(f)  # From csv I call the static method reader() and pass it a file object to create a reader object
+    header_row = next(reader)  # the next function receive a reader object a get the firs line of the file.
+    print(header_row)
+
+
