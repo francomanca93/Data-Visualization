7819b7a4803f71eacbb111a86da7b118540db2d2
francomanca93 <francomanca93@gmail.com>
2020-04-18 12:07:49 -0300

    Extracting and reading data

--- downloading-data/sitka_highs.py
+++ downloading-data/sitka_highs.py
@@ -7,7 +7,12 @@ with open(filename) as f:  # Open the file and assign the resulting file object 
     reader = csv.reader(f)  # From csv I call the static method reader() and pass it a file object to create a reader object
     header_row = next(reader)  # the next function receive a reader object a get the firs line of the file.
     
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # Get high temperatures from this file.
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+print(highs)
 
 
