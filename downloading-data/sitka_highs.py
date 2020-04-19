c513867c90732e3d1ad2c8d5a902e6e01fc14982
francomanca93 <francomanca93@gmail.com>
2020-04-19 07:55:37 -0300

    Plotting dates

--- downloading-data/sitka_highs.py
+++ downloading-data/sitka_highs.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -9,24 +10,29 @@ with open(filename) as f:  # Open the file and assign the resulting file object 
     reader = csv.reader(f)  # From csv I call the static method reader() and pass it a file object to create a reader object
     header_row = next(reader)  # the next function receive a reader object a get the firs line of the file.
     
-    # Get high temperatures from this file.
-    highs = []
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')  # convect date information to a datetime object
         high = int(row[5])
-        highs.append(high)
+        dates.append(current_date)  # Append date
+        highs.append(high)  # Append high temperature
 
-print(highs)
 
-# Plot the high temperatures.
+# Plot the high temperatures and its dates.
 plt.style.use('seaborn')
 fig = plt.subplot()
 ax = plt.subplot()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')  # I pass the dates and the high temperature values to plot()
 
 # Format plot
-plt.title("Daily high temperatures, July 2018", fontsize=24)
-plt.xlabel('Days', fontsize=16)
-plt.ylabel("Temperature(F)", fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=16)
+plt.title("Daily high temperatures, July 2018", fontsize=20)
+
+plt.xlabel('Dates', fontsize=10)
+plt.xticks(rotation=30)  # Rotate x ticks 30º
+
+plt.ylabel("Temperature(F)", fontsize=10)
+
+plt.tick_params(axis='both', which=None, labelsize=10)
 
 plt.show()
