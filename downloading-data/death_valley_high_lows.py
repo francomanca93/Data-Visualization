42ef8e9f58db8afd9d4a91ed5287cf606ea40757
francomanca93 <francomanca93@gmail.com>
2020-04-20 20:52:25 -0300

    Error checking

--- downloading-data/death_valley_high_lows.py
+++ downloading-data/death_valley_high_lows.py
@@ -1,10 +1,46 @@
 import csv
+from datetime import datetime
 
-filename = 'data/death_valley_2018_simple.csv'
+import matplotlib.pyplot as plt
+
+filename = '/home/franco/Documents/Python/Proyectos/Data-Visualization/downloading-data/data/death_valley_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    for index, column_header in enumerate(header_row):
-        print(index,column_header)
+    # Get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')  # convect date information to a datetime object
+        # I use a block try/except/else because there are values in blank and appear the following error by console
+        # ValueError: invalid literal for int() with base 10: ''
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)  # Append date
+            highs.append(high)  # Append high temperature
+            lows.append(low)  # Append low temperature
 
+# Plot the high and low temperatures and its dates.
+plt.style.use('seaborn')
+fig = plt.subplot()
+ax = plt.subplot()
+ax.plot(dates, highs, c='red', alpha=0.5)  # I pass the dates and the high temperature values to plot()
+ax.plot(dates, lows, c='blue', alpha=0.5)  # alpha controls a color's transparency
+plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.2)
+
+# Format plot
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
+
+plt.xlabel('Dates', fontsize=10)
+plt.xticks(rotation=30)  # Rotate x ticks 30º
+
+plt.ylabel("Temperature(F)", fontsize=10)
+
+plt.tick_params(axis='both', which=None, labelsize=10)
+
+plt.show()
