cd8187428124f5a9f7dad1fa50706931a2024b24
francomanca93 <francomanca93@gmail.com>
2020-04-20 19:05:32 -0300

    Adding new data sets and runnin code to see the headers

---
+++ downloading-data/death_valley_high_lows.py
@@ -0,0 +1,10 @@
+import csv
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    
+    for index, column_header in enumerate(header_row):
+        print(index,column_header)
+
