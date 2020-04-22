bd6c8f7cc8c0c49d2b69ba32ff79ea59eedf56a8
francomanca93 <francomanca93@gmail.com>
2020-04-22 10:59:36 -0300

    Extracting magnitudes

--- downloading-data/working_with_json_format/eq_explore_data.py
+++ downloading-data/working_with_json_format/eq_explore_data.py
@@ -5,6 +5,14 @@ filename = '/home/franco/Documents/Python/Proyectos/Data-Visualization/downloadi
 with open(filename) as f:
     all_eq_data = json.load(f)  # store entire set of data
 
-all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+# store all dictionaries
+all_eq_dicts = all_eq_data['features']  # I took the data associated with the key 'features' and store
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']  # I stored the mag data that it is conteined in 'properties' dictionary
+    mags.append(mag)  # Append the mag data
+
+print(mags[:10])  # Print the 10 first mag
+print(mags)
 
