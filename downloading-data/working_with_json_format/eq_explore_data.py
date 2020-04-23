ef90ee2636fb93186d885790464469b028be6de7
francomanca93 <francomanca93@gmail.com>
2020-04-23 06:58:52 -0300

    Specifying chart data in a different way

--- downloading-data/working_with_json_format/eq_explore_data.py
+++ downloading-data/working_with_json_format/eq_explore_data.py
@@ -22,7 +22,11 @@ for eq_dict in all_eq_dicts:
     lats.append(lat) # Append the latitudes data
 
 # Map the eathquakes.
-data = [Scattergeo(lon=lons, lat=lats)]  # I created the Scattergeo object inside the list data
+data = [{  # I created the Scattergeo object inside the list data. Inside the list the data is structured as key-value pairs.
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+}]
 my_layout = Layout(title='Global Earthquakes')  # I instance from a Layout class that it is conteined in plotly.graph_objs
 
 fig = {'data': data, 'layout': my_layout}  # Dictionary that conteins the data and the layout.
