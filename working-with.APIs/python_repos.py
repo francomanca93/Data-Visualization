4fbdd76aad756485764f5adf80c40accb4fb3779
francomanca93 <francomanca93@gmail.com>
2020-04-27 10:38:10 -0300

    Refactoring the response dictionary

--- working-with.APIs/python_repos.py
+++ working-with.APIs/python_repos.py
@@ -9,8 +9,6 @@ print(f"Status code: {r.status_code}")  # A status code of 200 indicates a succe
 # Store API response in a variable
 response_dict = r.json()  # The API returns the information in JSON format, so we use the json() method to convert the information to a Python dictionary
 
-# Process results.
-print(response_dict.keys())
 # Reposotories results
 print(f"Total repositories (from total_count): {response_dict['total_count']}")  # the total number of Python repositories on GitHub.
 
@@ -20,8 +18,13 @@ print(f"Repositories returned (from items): {len(repo_dicts)}")  # See how many 
 
 # Examine the first repository.
 repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")  # Print the number of keys to see how much information we have.
-for key in sorted(repo_dict.keys()):  # Print all the dictionary's keys to see what kind of information is included.
-    print(key)
 
+print("\nSelected information about first repository:")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
 
